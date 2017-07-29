671895fc19dc2ffd4d79aae31df040fa649180b8
squeemishly <squeemishly@gmail.com>
2017-07-29 14:18:20 -0600

    set up relationships between User and MetaDataPhotos, wrote test for FacebookPhotoWorker

--- app/models/meta_data_photo.rb
+++ app/models/meta_data_photo.rb
@@ -3,6 +3,7 @@ class MetaDataPhoto < ApplicationRecord
   has_many :fb_tags
   has_many :fb_reactions
   has_one :fb_place
+  belongs_to :user
 
   def self.photos_with_a_location(user)
     where(user_id: user.id).where.not(lat: nil)
