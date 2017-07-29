f8dc1fb6b2efbf99ab59a9708433350fbea713ec
squeemishly <squeemishly@gmail.com>
2017-07-29 16:26:52 -0600

    discovered and resolved lost merge conflict

--- app/workers/facebook_photo_worker.rb
+++ app/workers/facebook_photo_worker.rb
@@ -1,7 +1,6 @@
 class FacebookPhotoWorker
   include Sidekiq::Worker
 
-<<<<<<< HEAD
   def perform(user_id)
     user = User.find(user_id)
     Photos.get_data(user)
@@ -11,14 +10,4 @@ class FacebookPhotoWorker
   # def find_friends
   #
   # end
-=======
-  def perform(user)
-    Photos.get_data(user)
-    find_friends
-  end
-
-  def find_friends
-
-  end
->>>>>>> 3c3707829207ae928fbcebc5dfbe9c56a28fa8d1
 end
