9649edd355cbce645a0a28a2d6f3cf5084a23144
Carl Richmond <richmond.carl@outlook.com>
2017-07-31 16:46:58 -0600

    final check on output of api endpoints

--- spec/requests/api/v1/metadata/photos/facebook_multi_use_graph_spec.rb
+++ spec/requests/api/v1/metadata/photos/facebook_multi_use_graph_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "returns multi use dataset for bar and line graph" do
-    xscenario "gathers photo data by year" do
+    scenario "gathers photo data by year" do
       user = create(:user, fb_id: ENV['my_fb_id'], token: ENV['my_fb_token'])
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
@@ -10,7 +10,7 @@ RSpec.describe "returns multi use dataset for bar and line graph" do
       expect(result).to be_a Hash
   end
 
-  xscenario "gathers comment data by year" do
+  scenario "gathers comment data by year" do
       user = create(:user, fb_id: ENV['my_fb_id'], token: ENV['my_fb_token'])
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
@@ -19,7 +19,7 @@ RSpec.describe "returns multi use dataset for bar and line graph" do
       expect(result).to be_a Hash
   end
 
-  xscenario "API endpoint responds with data for photos" do
+  scenario "API endpoint responds with data for photos" do
       user = create(:user, fb_id: ENV['my_fb_id'], token: ENV['my_fb_token'])
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
       Photos.get_data(user)
