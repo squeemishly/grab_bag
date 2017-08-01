4f11512ef46e8676f311f84f8b7d62b3880f2bce
Adam Gunther <adamgunther1@gmail.com>
2017-08-01 15:45:44 -0600

    gets value to update on correct element since using slider for two different charts

--- app/assets/javascripts/map_locations.js
+++ app/assets/javascripts/map_locations.js
@@ -47,12 +47,6 @@ noUiSlider.create(stepSlider, {
 	}
 });
 
-var range = [
-  '2007','2008','2009','2010',
-  '2011','2012','2013','2014',
-  '2015','2016','2017'
-];
-
 var stepSliderValueElement = document.getElementById('slider-step-value');
 
 stepSlider.noUiSlider.on('update', function( values, handle ) {
