c14bbbe5525e1ca9fcd7f8b0212d8a7d7d450cb9
Carl Richmond <richmond.carl@outlook.com>
2017-07-31 20:17:54 -0600

    attempting to fix js charts

--- app/assets/javascripts/charts/fb_photo_comment_chart.js
+++ app/assets/javascripts/charts/fb_photo_comment_chart.js
@@ -1,9 +1,90 @@
-year = "2014";
-var ctx = document.getElementById('verbage_chart').getContext('2d');
-var myLineChart = new Chart(ctx, {
-    type: 'line',
-    data: data,
-    options: options
-});
+var canvas = document.getElementById("photos-comments");
+var ctx = canvas.getContext('2d');
 
-labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'Novemver', 'December']
+// Global Options:
+
+Chart.defaults.global.defaultFontColor = 'black';
+Chart.defaults.global.defaultFontSize = 16;
+
+var photo_values = function() {
+  return $.ajax({
+    url: '/api/v1/facebook/photos',
+    method: 'GET',
+  }).done(function(post){
+    Object.values(post);
+  })
+};
+
+var data = {
+  labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+  datasets: [{
+      label: "Stock A",
+      fill: false,
+      lineTension: 0.1,
+      backgroundColor: "rgba(225,0,0,0.4)",
+      borderColor: "red", // The main line color
+      borderCapStyle: 'square',
+      borderDash: [], // try [5, 15] for instance
+      borderDashOffset: 0.0,
+      borderJoinStyle: 'miter',
+      pointBorderColor: "black",
+      pointBackgroundColor: "white",
+      pointBorderWidth: 1,
+      pointHoverRadius: 8,
+      pointHoverBackgroundColor: "yellow",
+      pointHoverBorderColor: "brown",
+      pointHoverBorderWidth: 2,
+      pointRadius: 4,
+      pointHitRadius: 10,
+      // notice the gap in the data and the spanGaps: true
+      data: photo_values(),
+      spanGaps: true,
+    }, {
+      label: "Stock B",
+      fill: true,
+      lineTension: 0.1,
+      backgroundColor: "rgba(167,105,0,0.4)",
+      borderColor: "rgb(167, 105, 0)",
+      borderCapStyle: 'butt',
+      borderDash: [],
+      borderDashOffset: 0.0,
+      borderJoinStyle: 'miter',
+      pointBorderColor: "white",
+      pointBackgroundColor: "black",
+      pointBorderWidth: 1,
+      pointHoverRadius: 8,
+      pointHoverBackgroundColor: "brown",
+      pointHoverBorderColor: "yellow",
+      pointHoverBorderWidth: 2,
+      pointRadius: 4,
+      pointHitRadius: 10,
+      // notice the gap in the data and the spanGaps: false
+      data: [10, 20, 60, 95, 64, 78, 90,35,70,40,70,89],
+      spanGaps: false,
+    }
+
+  ]
+};
+
+// Notice the scaleLabel at the same level as Ticks
+var options = {
+  scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                },
+                scaleLabel: {
+                     display: true,
+                     labelString: 'Moola',
+                     fontSize: 20
+                  }
+            }]
+        }
+};
+
+// Chart declaration:
+var myBarChart = new Chart(ctx, {
+  type: 'line',
+  data: data,
+  options: options
+});
