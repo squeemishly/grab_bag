70778568c5986d2db1d696d31e84f6ad5a98aece
Carl Richmond <richmond.carl@outlook.com>
2017-07-31 21:18:26 -0600

    chart showing fb comments and photos now displays

--- app/assets/javascripts/charts/commonwealth.js
+++ app/assets/javascripts/charts/commonwealth.js
@@ -1,9 +1,9 @@
-// $.ajax({
-//     url: ("/api/v1/file/american"),
-//     type: 'GET',
-//     success: function(data) {
-//         var json_data = data;
-//         var values = Object.values(json_data);
-//         document.getElementById('commonwealth').innerHTML += values;
-//     }
-// });
+$.ajax({
+    url: ("/api/v1/file/american"),
+    type: 'GET',
+    success: function(data) {
+        var json_data = data;
+        var values = Object.values(json_data);
+        document.getElementById('commonwealth').innerHTML += values;
+    }
+});
