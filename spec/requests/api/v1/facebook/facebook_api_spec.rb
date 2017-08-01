22721c27a4e09e3b68f2a4278b96c690bf1762bf
squeemishly <squeemishly@gmail.com>
2017-08-01 15:01:38 -0600

    fixes test to include facebook user information

--- spec/requests/api/v1/facebook/facebook_api_spec.rb
+++ spec/requests/api/v1/facebook/facebook_api_spec.rb
@@ -2,24 +2,24 @@ require 'rails_helper'
 
 RSpec.describe "facebook api" do
   it "can display a list of commenters" do
-    user = create(:user)
-    user2 = create(:user)
+    user = create(:user, fb_id: 7)
+    user2 = create(:user, fb_id: 10)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
     mdp = MetaDataPhoto.create(user_id: user.id)
-    comment1 = FbComment.create(fb_uname: "squee", user_id: user.id, meta_data_photo_id: mdp.id)
-    comment2 = FbComment.create(fb_uname: "Danny", meta_data_photo_id: mdp.id, user_id: user.id)
-    comment3 = FbComment.create(fb_uname: "Danny", meta_data_photo_id: mdp.id, user_id: user.id)
-    comment4 = FbComment.create(fb_uname: "Sheila", meta_data_photo_id: mdp.id, user_id: user.id)
-    comment5 = FbComment.create(fb_uname: "Elfton", meta_data_photo_id: mdp.id, user_id: user.id)
-    comment6 = FbComment.create(fb_uname: "Elfton", meta_data_photo_id: mdp.id, user_id: user.id)
-    comment7 = FbComment.create(fb_uname: "Elfton", meta_data_photo_id: mdp.id, user_id: user.id)
-    comment8 = FbComment.create(fb_uname: "Natalia", meta_data_photo_id: mdp.id, user_id: user.id)
-    comment9 = FbComment.create(fb_uname: "Natalia", meta_data_photo_id: mdp.id, user_id: user.id)
-    comment10 = FbComment.create(fb_uname: "Natalia", meta_data_photo_id: mdp.id, user_id: user.id)
-    comment11 = FbComment.create(fb_uname: "Natalia", meta_data_photo_id: mdp.id, user_id: user.id)
-    comment12 = FbComment.create(fb_uname: "Purple", meta_data_photo_id: mdp.id, user_id: user.id)
-    comment13 = FbComment.create(fb_uname: "Purple", meta_data_photo_id: mdp.id, user_id: user2.id)
+    comment1 = FbComment.create(fb_uname: "squee", user_id: user.id, meta_data_photo_id: mdp.id, fb_uid: 6)
+    comment2 = FbComment.create(fb_uname: "Danny", meta_data_photo_id: mdp.id, user_id: user.id, fb_uid: 5)
+    comment3 = FbComment.create(fb_uname: "Danny", meta_data_photo_id: mdp.id, user_id: user.id, fb_uid: 5)
+    comment4 = FbComment.create(fb_uname: "Sheila", meta_data_photo_id: mdp.id, user_id: user.id, fb_uid: 4)
+    comment5 = FbComment.create(fb_uname: "Elfton", meta_data_photo_id: mdp.id, user_id: user.id, fb_uid: 3)
+    comment6 = FbComment.create(fb_uname: "Elfton", meta_data_photo_id: mdp.id, user_id: user.id, fb_uid: 3)
+    comment7 = FbComment.create(fb_uname: "Elfton", meta_data_photo_id: mdp.id, user_id: user.id, fb_uid: 3)
+    comment8 = FbComment.create(fb_uname: "Natalia", meta_data_photo_id: mdp.id, user_id: user.id, fb_uid: 2)
+    comment9 = FbComment.create(fb_uname: "Natalia", meta_data_photo_id: mdp.id, user_id: user.id, fb_uid: 2)
+    comment10 = FbComment.create(fb_uname: "Natalia", meta_data_photo_id: mdp.id, user_id: user.id, fb_uid: 2)
+    comment11 = FbComment.create(fb_uname: "Natalia", meta_data_photo_id: mdp.id, user_id: user.id, fb_uid: 2)
+    comment12 = FbComment.create(fb_uname: "Purple", meta_data_photo_id: mdp.id, user_id: user.id, fb_uid: 1)
+    comment13 = FbComment.create(fb_uname: "Purple", meta_data_photo_id: mdp.id, user_id: user2.id, fb_uid: 1)
 
     get '/api/v1/facebook/facebook_commenters'
 
@@ -35,24 +35,24 @@ RSpec.describe "facebook api" do
   end
 
   it "can display a list of taggers" do
-    user = create(:user)
-    user2 = create(:user)
+    user = create(:user, fb_id: 7)
+    user2 = create(:user, fb_id: 10)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
     mdp = MetaDataPhoto.create(user_id: user.id)
-    comment1 = FbTag.create(fb_uname: "squee", user_id: user.id, meta_data_photo_id: mdp.id)
-    comment2 = FbTag.create(fb_uname: "Danny", meta_data_photo_id: mdp.id, user_id: user.id)
-    comment3 = FbTag.create(fb_uname: "Danny", meta_data_photo_id: mdp.id, user_id: user.id)
-    comment4 = FbTag.create(fb_uname: "Sheila", meta_data_photo_id: mdp.id, user_id: user.id)
-    comment5 = FbTag.create(fb_uname: "Elfton", meta_data_photo_id: mdp.id, user_id: user.id)
-    comment6 = FbTag.create(fb_uname: "Elfton", meta_data_photo_id: mdp.id, user_id: user.id)
-    comment7 = FbTag.create(fb_uname: "Elfton", meta_data_photo_id: mdp.id, user_id: user.id)
-    comment8 = FbTag.create(fb_uname: "Natalia", meta_data_photo_id: mdp.id, user_id: user.id)
-    comment9 = FbTag.create(fb_uname: "Natalia", meta_data_photo_id: mdp.id, user_id: user.id)
-    comment10 = FbTag.create(fb_uname: "Natalia", meta_data_photo_id: mdp.id, user_id: user.id)
-    comment11 = FbTag.create(fb_uname: "Natalia", meta_data_photo_id: mdp.id, user_id: user.id)
-    comment12 = FbTag.create(fb_uname: "Purple", meta_data_photo_id: mdp.id, user_id: user.id)
-    comment13 = FbTag.create(fb_uname: "Purple", meta_data_photo_id: mdp.id, user_id: user2.id)
+    tag1 = FbTag.create(fb_uname: "squee", user_id: user.id, meta_data_photo_id: mdp.id, fb_uid: 6)
+    tag2 = FbTag.create(fb_uname: "Danny", meta_data_photo_id: mdp.id, user_id: user.id, fb_uid: 5)
+    tag3 = FbTag.create(fb_uname: "Danny", meta_data_photo_id: mdp.id, user_id: user.id, fb_uid: 5)
+    tag4 = FbTag.create(fb_uname: "Sheila", meta_data_photo_id: mdp.id, user_id: user.id, fb_uid: 4)
+    tag5 = FbTag.create(fb_uname: "Elfton", meta_data_photo_id: mdp.id, user_id: user.id, fb_uid: 3)
+    tag6 = FbTag.create(fb_uname: "Elfton", meta_data_photo_id: mdp.id, user_id: user.id, fb_uid: 3)
+    tag7 = FbTag.create(fb_uname: "Elfton", meta_data_photo_id: mdp.id, user_id: user.id, fb_uid: 3)
+    tag8 = FbTag.create(fb_uname: "Natalia", meta_data_photo_id: mdp.id, user_id: user.id, fb_uid: 2)
+    tag9 = FbTag.create(fb_uname: "Natalia", meta_data_photo_id: mdp.id, user_id: user.id, fb_uid: 2)
+    tag10 = FbTag.create(fb_uname: "Natalia", meta_data_photo_id: mdp.id, user_id: user.id, fb_uid: 2)
+    tag11 = FbTag.create(fb_uname: "Natalia", meta_data_photo_id: mdp.id, user_id: user.id, fb_uid: 2)
+    tag12 = FbTag.create(fb_uname: "Purple", meta_data_photo_id: mdp.id, user_id: user.id, fb_uid: 1)
+    tag13 = FbTag.create(fb_uname: "Purple", meta_data_photo_id: mdp.id, user_id: user2.id, fb_uid: 1)
 
     get '/api/v1/facebook/facebook_taggers'
 
@@ -68,24 +68,24 @@ RSpec.describe "facebook api" do
   end
 
   it "can display a list of likers" do
-    user = create(:user)
-    user2 = create(:user)
+    user = create(:user, fb_id: 7)
+    user2 = create(:user, fb_id: 10)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
     mdp = MetaDataPhoto.create(user_id: user.id)
-    comment1 = FbReaction.create(fb_uname: "squee", user_id: user.id, meta_data_photo_id: mdp.id)
-    comment2 = FbReaction.create(fb_uname: "Danny", meta_data_photo_id: mdp.id, user_id: user.id)
-    comment3 = FbReaction.create(fb_uname: "Danny", meta_data_photo_id: mdp.id, user_id: user.id)
-    comment4 = FbReaction.create(fb_uname: "Sheila", meta_data_photo_id: mdp.id, user_id: user.id)
-    comment5 = FbReaction.create(fb_uname: "Elfton", meta_data_photo_id: mdp.id, user_id: user.id)
-    comment6 = FbReaction.create(fb_uname: "Elfton", meta_data_photo_id: mdp.id, user_id: user.id)
-    comment7 = FbReaction.create(fb_uname: "Elfton", meta_data_photo_id: mdp.id, user_id: user.id)
-    comment8 = FbReaction.create(fb_uname: "Natalia", meta_data_photo_id: mdp.id, user_id: user.id)
-    comment9 = FbReaction.create(fb_uname: "Natalia", meta_data_photo_id: mdp.id, user_id: user.id)
-    comment10 = FbReaction.create(fb_uname: "Natalia", meta_data_photo_id: mdp.id, user_id: user.id)
-    comment11 = FbReaction.create(fb_uname: "Natalia", meta_data_photo_id: mdp.id, user_id: user.id)
-    comment12 = FbReaction.create(fb_uname: "Purple", meta_data_photo_id: mdp.id, user_id: user.id)
-    comment13 = FbReaction.create(fb_uname: "Purple", meta_data_photo_id: mdp.id, user_id: user2.id)
+    reaction1 = FbReaction.create(fb_uname: "squee", user_id: user.id, meta_data_photo_id: mdp.id, fb_uid: 6)
+    reaction2 = FbReaction.create(fb_uname: "Danny", meta_data_photo_id: mdp.id, user_id: user.id, fb_uid: 5)
+    reaction3 = FbReaction.create(fb_uname: "Danny", meta_data_photo_id: mdp.id, user_id: user.id, fb_uid: 5)
+    reaction4 = FbReaction.create(fb_uname: "Sheila", meta_data_photo_id: mdp.id, user_id: user.id, fb_uid: 4)
+    reaction5 = FbReaction.create(fb_uname: "Elfton", meta_data_photo_id: mdp.id, user_id: user.id, fb_uid: 3)
+    reaction6 = FbReaction.create(fb_uname: "Elfton", meta_data_photo_id: mdp.id, user_id: user.id, fb_uid: 3)
+    reaction7 = FbReaction.create(fb_uname: "Elfton", meta_data_photo_id: mdp.id, user_id: user.id, fb_uid: 3)
+    reaction8 = FbReaction.create(fb_uname: "Natalia", meta_data_photo_id: mdp.id, user_id: user.id, fb_uid: 2)
+    reaction9 = FbReaction.create(fb_uname: "Natalia", meta_data_photo_id: mdp.id, user_id: user.id, fb_uid: 2)
+    reaction10 = FbReaction.create(fb_uname: "Natalia", meta_data_photo_id: mdp.id, user_id: user.id, fb_uid: 2)
+    reaction11 = FbReaction.create(fb_uname: "Natalia", meta_data_photo_id: mdp.id, user_id: user.id, fb_uid: 2)
+    reaction12 = FbReaction.create(fb_uname: "Purple", meta_data_photo_id: mdp.id, user_id: user.id, fb_uid: 1)
+    reaction13 = FbReaction.create(fb_uname: "Purple", meta_data_photo_id: mdp.id, user_id: user2.id, fb_uid: 1)
 
     get '/api/v1/facebook/facebook_reactions'
 
