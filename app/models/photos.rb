69253b4c756bac4ff44b0b2950d2fba7d18de429
squeemishly <squeemishly@gmail.com>
2017-08-01 17:24:19 -0600

    remove commented out code

--- app/models/photos.rb
+++ app/models/photos.rb
@@ -7,68 +7,20 @@ class Photos
 
         if photo[:place]
           place = self.create_place(photo)
-          # place = FbPlace.find_or_create_by(fb_place_id: photo[:place][:id]) do |place|
-          #   place.name = photo[:place][:name]
-          #   place.city = photo[:place][:location][:city] rescue nil
-          #   place.country = photo[:place][:location][:country] rescue nil
-          #   place.lat = photo[:place][:location][:latitude] rescue nil
-          #   place.long = photo[:place][:location][:longitude] rescue nil
-          # end
         end
 
         meta_data = self.create_metadata(photo, place, user)
 
-        # meta_data = MetaDataPhoto.create!(image: photo[:images][0][:source],
-        #                 created_time: photo[:created_time],
-        #                 fb_uname: photo[:from][:name],
-        #                 fb_uid: photo[:from][:id],
-        #                 fb_photo_id: photo[:id],
-        #                 fb_place_id: place.try(:id),
-        #                 user_id: user.id,
-        #                 )
-        #
-        # if photo[:place] && place.lat != nil
-        #   meta_data.update_attributes(lat: place.lat)
-        # end
-        # if photo[:place] && place.long != nil
-        #   meta_data.update_attributes(long: place.long)
-        # end
-
         if photo[:reactions]
           self.create_reactions(photo, meta_data, user)
-          # photo[:reactions][:data].each do |reaction|
-          #   FbReaction.create(fb_uid: reaction[:id],
-          #                     fb_uname: reaction[:name],
-          #                     reaction_type: reaction[:type],
-          #                     meta_data_photo_id: meta_data.id,
-          #                     user_id: user.id
-          #                     )
-          # end
         end
 
         if photo[:tags]
           self.create_tags(photo, meta_data, user)
-          # photo[:tags][:data].each do |tag|
-          #   FbTag.create(fb_uid: tag[:id],
-          #                 fb_uname: tag[:name],
-          #                 meta_data_photo_id: meta_data.id,
-          #                 user_id: user.id
-          #                 )
-          # end
         end
 
         if photo[:comments]
           self.create_comments(photo, meta_data, user)
-          # photo[:comments][:data].each do |comment|
-          #   FbComment.create(like_count: comment[:like_count],
-          #                     comment_count: comment[:comment_count],
-          #                     message: comment[:message],
-          #                     fb_uname: comment[:from][:name],
-          #                     fb_uid: comment[:from][:id],
-          #                     meta_data_photo_id: meta_data.id,
-          #                     user_id: user.id
-          #                     )
-          # end
         end
 
       end
